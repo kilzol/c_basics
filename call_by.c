7e864977ede911771b0c98c96c3b53431e2b4c9e
kilzol007 <40137450+kilzol@users.noreply.github.com>
2019-09-07 20:52:23 +0530

    simple program to understand call by value and call by refrence

---
+++ call_by.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int swapv(int x,int y);
+int swapr(int *x,int *y);
+int main()
+{int x,y,z;
+printf("Enter two number and enter 1 for swapping by call by value and 2 for swapping by call by value \n");
+scanf("%d%d %d",&x,&y,&z);
+
+if(z==1){
+	
+swapv(x,y);	
+}
+else{
+swapr(&x,&y);
+printf("value at address is changed in this case a=%d and b=%d",*(&x),*(&y));
+}
+return 0;
+}
+
+int swapv(int x,int y)
+{int t;
+t=x;
+x=y;
+y=t;
+printf(" value at address does not change a=%d and b=%d",x,y);
+}
+
+int swapr(int *x,int *y)
+{
+int t;
+t=*x;
+*x=*y;
+*y=t;
+
+}
